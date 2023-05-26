b9314dea62556fd5a8912b1ab831209066cad952
lina <linacodes@yahoo.com>
2023-05-26 22:51:37 +0200

    things KINDA work as intended but not really YET

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+  initNavigation();
+});
+
 function initNavigation() {
-    const sections = document.getElementsByClassName('section');
-    let currentSectionIndex = 0;
-  
-    // Display the initial section
-    sections[currentSectionIndex].classList.add('active');
-  
-    // Keyboard navigation
-    document.addEventListener('keydown', (event) => {
-      if (event.key === 'ArrowLeft') {
-        navigateToSection(currentSectionIndex - 1);
-      } else if (event.key === 'ArrowRight') {
-        navigateToSection(currentSectionIndex + 1);
-      }
+  const sections = document.getElementsByClassName('section');
+  const mainMenuButtons = document.getElementsByClassName('menu-button');
+  const sideMenuButtons = document.querySelectorAll('#portfolio .menu-button[data-section^="project"]');
+  let currentSectionIndex = 0;
+
+  // Display the initial section
+  sections[currentSectionIndex].classList.add('active');
+
+  // Main menu button click navigation
+  for (let i = 0; i < mainMenuButtons.length; i++) {
+    mainMenuButtons[i].addEventListener('click', () => {
+      const sectionId = mainMenuButtons[i].dataset.section;
+      const sectionIndex = Array.from(sections).findIndex((section) => section.id === sectionId);
+      navigateToSection(sectionIndex);
     });
-  
-    // Button click navigation
-    const menuButtons = document.getElementsByClassName('menu-button');
-    for (let i = 0; i < menuButtons.length; i++) {
-      menuButtons[i].addEventListener('click', () => {
-        navigateToSection(i);
-      });
-    }
-  
-    function navigateToSection(index) {
-      if (index < 0 || index >= sections.length) {
-        return;
-      }
-  
-      sections[currentSectionIndex].classList.remove('active');
-      currentSectionIndex = index;
-      sections[currentSectionIndex].classList.add('active');
-    }
   }
-  
+
+  // Side menu button click navigation
+  for (let i = 0; i < sideMenuButtons.length; i++) {
+    sideMenuButtons[i].addEventListener('click', () => {
+      const sectionId = sideMenuButtons[i].dataset.section;
+      const sectionIndex = Array.from(sections).findIndex((section) => section.id === sectionId);
+      navigateToSection(sectionIndex);
+    });
+  }
+
+  function navigateToSection(index) {
+    if (index < 0 || index >= sections.length) {
+      return;
+    }
+
+    sections[currentSectionIndex].classList.remove('active');
+    currentSectionIndex = index;
+    sections[currentSectionIndex].classList.add('active');
+  }
+}
